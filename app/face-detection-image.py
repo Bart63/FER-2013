3ce5b08f15ce262c824700bedd91a151ec4111de
Kerinjo <34321962+Kerinjo@users.noreply.github.com>
2021-09-30 21:27:45 +0200

    Face detection from image

---
+++ app/face-detection-image.py
@@ -0,0 +1,31 @@
+import cv2
+import sys
+
+# cl arguments
+imagePath = sys.argv[1]
+cascPath = sys.argv[2]
+
+# haar cascade creation
+haarCascade = cv2.CascadeClassifier(cascPath)
+
+# read image and convert it to grayscale
+img = cv2.imread(imagePath)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# applying haar cascade to grayscale image
+faces = haarCascade.detectMultiScale(
+    gray, 
+    scaleFactor=1.1,
+    minNeighbors=5,
+    flags = cv2.CASCADE_SCALE_IMAGE,
+    minSize=(48, 48)
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Mordy", img)
+cv2.waitKey(0)
